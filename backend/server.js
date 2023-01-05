95ecf2679b44a0225d80344d5c786b7c5876e80f
Raghav Kasyap <raghavk2503@gmail.com>
2023-01-05 18:26:37 +0530

    express error handling

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ import dotenv from 'dotenv'
 import chalk from 'chalk'
 
 import connectDB from './config/db.js'
+import userRoutes from './routes/userRoutes.js'
+import AppError from './utils/appError.js'
+import { globalErrorController } from './controllers/errorController.js'
 
 dotenv.config()
 
@@ -10,9 +13,22 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+
+app.use('/api/v1/users', userRoutes)
+
 app.get('/', (req, res) => {
 	res.send('API is running...')
 })
+
+app.all('*', (req, res, next) => {
+	const err = new AppError(`Can't find URL ${req.originalUrl} on this server`, 404)
+
+	next(err)
+})
+
+// Error Handling Middleware
+app.use(globalErrorController)
 
 const port = process.env.PORT | 5000
 
